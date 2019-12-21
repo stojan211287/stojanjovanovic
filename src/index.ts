54e080b4b1d0bb4c425856f51208d7e984ccc0da
stojan211287 <whyisepsilonneverlessthanzero@gmail.com>
2019-12-21 13:30:48 +0000

    change font size and tile canvas anchors

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,19 @@ import './styles.scss';
 import { noise } from './noisejs/perlin.js';
 
 // Define starting seed for Perlin noise
-var seed = 1337;
+var seed = 567122400;
 
+// Perlin noise params
+const perlinScale = 0.01;
+const perlinStrength = 20;
+
+// Prepare canvas and context
 const canvases = document.getElementsByTagName("canvas") as []HTMLCanvasElement;
 
 const canvas = canvases[0];
 const context = canvas.getContext("2d");
 
-context.lineWidth = 25;
+context.lineWidth = 20;
 context.strokeStyle = "#FF0000";
 
 function updateCanvasSize() {
@@ -19,6 +24,7 @@ function updateCanvasSize() {
     canvas.height = window.innerHeight;
 }
 
+/* Hexagon drawing function */ 
 function hexagon(x: number, y: numer, r: number) {
   let angle = 0;
   for(let i = 0; i < 6; i ++) {
@@ -31,15 +37,22 @@ function hexagon(x: number, y: numer, r: number) {
 
 /* Function that takes a point coordinate and returns a Perlin-noise-perturbed point */
 function perlinize(x: number, y: number) {
-  const scale = 0.01;
-  const strength = 20;
  
-  const angle = noise.perlin2(x * scale, y * scale) * Math.PI;
+  const angle = noise.perlin2(x * perlinScale, y * perlinScale) * Math.PI;
 
   return {
-    x: x + Math.cos(angle) * strength,
-    y: y + Math.sin(angle) * strength,
+    x: x + Math.cos(angle) * perlinStrength,
+    y: y + Math.sin(angle) * perlinStrength,
   }
+}
+
+/* Hexagon shading function */
+function shade(x: number, y: number) {
+  // noise.perlin2 returns a number in {-1, 1}, so we need to re-scale to {0, 1}
+  const shadeValue = ( noise.perlin2(x * perlinScale, y * perlinScale) + 1 ) / 2;
+  // Re-scale shade to [0, 255]
+  const shadeRGB = 255 - shadeValue*255;
+  return "rgb(" + shadeRGB + "," + shadeRGB + "," + shadeRGB + ")";
 }
 
 /* Background drawing function */
@@ -55,16 +68,18 @@ function drawTiles(seed: number = seed) {
   const ydelta = Math.sin(Math.PI / 3) * radius;
   let even = true;
 
-  for(let y = 0; y < window.innerHeight; y += ydelta) {
+  for(let y = -ydelta; y < window.innerHeight + ydelta; y += ydelta) {
     let offset = 0;
     let yy = y;
     if(even) {
       offset = radius * 1.5;
     }
     
-    for(let x = 0; x < window.innerWidth; x += radius * 3) {
+    for(let x = -radius * 3; x < window.innerWidth + radius *  4; x += radius * 3) {
       context.beginPath();
       hexagon(x + offset, y, radius);
+      context.fillStyle = shade(x + offset, y);
+      context.fill();
       context.stroke();
     }
     even = !even;
